959326892a8018018228935551863cf1ccdfd58f
Hera Silveira Ramos <hera.ramos01@gmail.com>
2026-05-14 19:23:39 -0300

    criando livros.ts

--- src/modulos/autores/autores.repository.ts
+++ src/modulos/autores/autores.repository.ts
@@ -3,10 +3,10 @@ import {
   Injectable,
   InternalServerErrorException,
 } from '@nestjs/common';
-import { DRIZZLE } from 'src/db/database/database.constants';
-import type { DrizzleDB } from 'src/db/types/drizzleDB';
-import { autoresTabela } from 'src/db/schemas';
 import { eq } from 'drizzle-orm';
+import { DRIZZLE } from 'src/db/database/database.constants';
+import { autoresTabela } from 'src/db/schemas';
+import type { DrizzleDB } from 'src/db/types/drizzleDB';
 import { AtualizarAutorDto, CriarAutorDto } from './autores.dto';
 
 @Injectable()
@@ -14,11 +14,7 @@ export class AutoresRepository {
   constructor(@Inject(DRIZZLE) private readonly db: DrizzleDB) {}
 
   async listarAutores() {
-    try {
-      return this.db.select().from(autoresTabela);
-    } catch (error) {
-      throw new InternalServerErrorException('Erro ao listar Autores');
-    }
+    return this.db.select().from(autoresTabela);
   }
 
   async listarAutor(id: number) {
@@ -27,38 +23,50 @@ export class AutoresRepository {
         .select()
         .from(autoresTabela)
         .where(eq(autoresTabela.id, id));
-
       return autorEncontrado[0];
     } catch (error) {
       throw new InternalServerErrorException('Erro ao listar um autor');
     }
   }
-
   async criarAutor(bodyRequest: CriarAutorDto) {
     try {
       await this.db.insert(autoresTabela).values(bodyRequest);
-
       const autorCriado = await this.db
         .select()
         .from(autoresTabela)
-        .where(eq(autoresTabela.email, bodyRequest.email)); //erro//
-
+        .where(eq(autoresTabela.email, bodyRequest.email));
       return autorCriado;
     } catch (error) {
-      throw new InternalServerErrorException('Erro ao criar um autor');
+      throw new InternalServerErrorException('Erro ao criar autor.');
     }
   }
 
   async atualizarAutor(id: number, bodyRequest: AtualizarAutorDto) {
     try {
-      await this.db
+      const autorAtualizado = await this.db
         .update(autoresTabela)
         .set(bodyRequest)
         .where(eq(autoresTabela.id, id));
-
-      return 'Autor atualizado com sucesso';
+      return `Autor atualizado com sucesso: ${autorAtualizado}`;
     } catch (error) {
-      throw new InternalServerErrorException('Erro ao atualizar um autor');
+      throw new InternalServerErrorException('Erro ao atualizar autor.');
     }
   }
+
+  async deletarAutor(id: number) {
+    try {
+      await this.db.delete(autoresTabela).where(eq(autoresTabela.id, id));
+      return `Autor deletado com sucesso: ${id}`;
+    } catch (error) {
+      throw new InternalServerErrorException('Erro ao deletar autor.');
+    }
+  }
+
+  // async inativarAutor(id: number) {
+  //   try {
+  //     await this.db.update(autoresTabela).set({ ativo: false }).where(eq(autoresTabela.id, id));
+  //     return `Autor inativado com sucesso: ${id}`;
+  //   } catch (error) {
+  //     throw new InternalServerErrorException('Erro ao inativar autor.');
+  //   }
 }
--- src/modulos/autores/autores.service.ts
+++ src/modulos/autores/autores.service.ts
@@ -30,19 +30,17 @@ export class AutoresService {
   constructor(private readonly autoresRepository: AutoresRepository) {}
 
   async listarAutores() {
-    return this.autoresRepository.listarAutores();
+    return await this.autoresRepository.listarAutores();
   }
 
   async listarAutor(id: number) {
     const autorEncontrado = await this.autoresRepository.listarAutor(id);
-
+    //if (!autorEncontrado) <-- desta forma ele mostra um array vazio
     if (!autorEncontrado) {
-      throw new NotFoundException(`Autor com id ${id} não encontrado`);
+      throw new NotFoundException(`Autor com id ${id} não encontrado.`);
     }
-
     return autorEncontrado;
   }
-
   criarAutor(bodyRequest: CriarAutorDto) {
     return this.autoresRepository.criarAutor(bodyRequest);
   }
@@ -50,9 +48,68 @@ export class AutoresService {
   async atualizarAutor(idAutor: number, bodyRequest: AtualizarAutorDto) {
     await this.listarAutor(idAutor);
 
-    return await this.autoresRepository.atualizarAutor(idAutor, bodyRequest);
+    return this.autoresRepository.atualizarAutor(idAutor, bodyRequest);
   }
 
+  async deletarAutor(idAutor: number) {
+    await this.listarAutor(idAutor);
+    return this.autoresRepository.deletarAutor(idAutor);
+  }
+
+  /*----------MÉTODOS USANDO ARRAY SEM DB------------------
+  listarAutores() {
+    if (!autores) {
+      return 'Não há autores cadastrados';
+    }
+    //console.log("listarService");
+    //API sempre retorna json
+    return autores;
+  }
+
+  listarAutor(id: number) {
+    const autorEncontrado = autores.find((autor) => autor.id === id);
+    if (!autorEncontrado) throw new NotFoundException('Autor não encontrado');
+
+    return autorEncontrado;
+  }
+    
+  criarAutor(bodyRequest: CriarAutorDto) {
+    if (!bodyRequest.nome || !bodyRequest.email) {
+      return 'Nome e email são obrigatórios.';
+    }
+    autores.push({
+      id: autores.length + 1,
+      nome: bodyRequest.nome,
+      email: bodyRequest.email,
+    });
+    return autores;
+  }
+
+  ----------MÉTODOS USANDO ARRAY SEM DB------------------
+  atualizarAutor(idAutor: number, bodyRequest: AtualizarAutorDto) {
+    //const autorEncontrado = autores.find((autor) => autor.id === idAutor);
+    const autorEncontrado = this.listarAutor(idAutor);
+
+    /*chamando o método listarAutor, economizando as próximas 3 linhas de código
+
+    if (!autorEncontrado) {
+      return 'Autor não encontrado.';
+    }
+
+    if (!bodyRequest.nome && !bodyRequest.email) {
+      throw new BadRequestException('Nome ou email é obrigatório.');
+    }
+
+    if (bodyRequest.nome) {
+      autorEncontrado.nome = bodyRequest.nome;
+    }
+    if (bodyRequest.email) {
+      autorEncontrado.email = bodyRequest.email;
+    }
+
+    return autorEncontrado;
+  }
+  
   deletarAutor(idAutor: number) {
     // utiliza o this para referenciar a própria classe dele(AutoresService)
     // os metódos pertencem ao AutoresService
@@ -63,4 +120,13 @@ export class AutoresService {
 
     return autores;
   }
+  */
+
+  // async inativarAutor(id: number) {
+  //   try {
+  //     await this.db.update(autoresTabela).set({ ativo: false }).where(eq(autoresTabela.id, id));
+  //     return `Autor inativado com sucesso: ${id}`;
+  //   } catch (error) {
+  //     throw new InternalServerErrorException('Erro ao inativar autor.');
+  //   }
 }
