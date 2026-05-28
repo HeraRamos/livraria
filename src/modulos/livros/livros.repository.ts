b7942d307c9f2ca38facebe17c2bf81791f2944e
Hera Silveira Ramos <hera.ramos01@gmail.com>
2026-05-28 19:24:41 -0300

    teste

--- src/modulos/livros/livros.repository.ts
+++ src/modulos/livros/livros.repository.ts
@@ -7,6 +7,7 @@ import { DRIZZLE } from 'src/db/database/database.constants';
 import type { DrizzleDB } from 'src/db/types/drizzleDB';
 import { livrosTabela } from 'src/db/schemas/livros';
 import { CriarLivroDto } from './livros.dto';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class LivrosRepository {
@@ -33,5 +34,16 @@ export class LivrosRepository {
       return `Livro ${bodyRequest.titulo} criado com sucesso`;
     } catch (error) {}
     throw new InternalServerErrorException('Erro ao criar livro');
+  }
+  async listarlivro(id: number) {
+    try {
+      const livroEncontrado = await this.db
+        .select()
+        .from(livrosTabela)
+        .where(eq(livrosTabela.id, id));
+      return livroEncontrado[0];
+    } catch (error) {
+      throw new InternalServerErrorException('Erro ao listar um livro');
+    }
   }
 }
